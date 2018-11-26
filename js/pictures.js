ee53b05313df675d1871cbcac0f07a0654226477
Morfey216 <Morfey4web@gmail.com>
2018-11-26 03:11:10 +0300

    Добавление генерации исходных данных

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var MIN_LIKE = 15;
+var MAX_LIKE = 200;
+var MIN_COMMENT = 1;
+var MAX_COMMENT = 12;
+
 var commentsSamples = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -18,19 +23,27 @@ var descriptionsSamples = [
   'Вот это тачка!'
 ];
 
-var usersPictures = [
-  {
-    url: '',
-    likes: 0,
-    comments: [],
-    description: ''
+var usersPictures = [];
+
+var getIndex = function (maxIndex, minIndex) {
+  return Math.floor(Math.random() * (maxIndex - minIndex)) + minIndex;
+};
+
+var createComments = function () {
+  var commentsKit = [];
+  var commentQuantity = getIndex(MAX_COMMENT, MIN_COMMENT);
+
+  for (var index = 0; index <= commentQuantity; index++) {
+    commentsKit[index] = commentsSamples[getIndex(commentsSamples.length, 0)];
   }
-];
+
+  return commentsKit;
+};
 
 for (var i = 0; i < 25; i++) {
   usersPictures[i] = {};
   usersPictures[i].url = 'photos/' + (i + 1) + '.jpg';
-  usersPictures[i].likes = i;
-  usersPictures[i].comments = commentsSamples[3];
-  usersPictures[i].description = descriptionsSamples[1];
+  usersPictures[i].likes = getIndex(MAX_LIKE + 1, MIN_LIKE);
+  usersPictures[i].comments = createComments();
+  usersPictures[i].description = descriptionsSamples[getIndex(descriptionsSamples.length, 0)];
 }
