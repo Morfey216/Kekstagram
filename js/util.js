1f124476dac12fcd7397ed9e4bf67ac123d15e0e
Morfey216 <Morfey4web@gmail.com>
2019-01-03 22:24:52 +0300

    Оптимизация перечисления кейкодов

--- js/util.js
+++ js/util.js
@@ -1,17 +1,19 @@
 'use strict';
 
 (function () {
-  var ESC_KEYCODE = 27;
-  var ENTER_KEYCODE = 13;
+  var Keycode = {
+    ESC: 27,
+    ENTER: 13
+  };
 
   window.util = {
     isEscEvent: function (evt, action) {
-      if (evt.keyCode === ESC_KEYCODE) {
+      if (evt.keyCode === Keycode.ESC) {
         action();
       }
     },
     isEnterEvent: function (evt, action) {
-      if (evt.keyCode === ENTER_KEYCODE) {
+      if (evt.keyCode === Keycode.ENTER) {
         action();
       }
     },
